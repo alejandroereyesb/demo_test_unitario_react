21c2542316603647826143ff44c5977a01901421
mario-olopez <mariolopezclav@gmail.com>
2025-05-27 09:58:43 +0200

    add/error-test

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import Button from "./components/Button";
 
 function App() {
   const [count, setCount] = useState(0);
-  let a;
 
+  
   const incrementCount = (increment) => {
     setCount(count + increment);
   };
