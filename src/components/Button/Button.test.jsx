85edae4cc1c9b13a64fafc97b93b6410d76e1118
Alejandro Reyes <reyesbalejandroe@gmail.com>
2025-12-09 11:00:49 +0100

    add/error

--- src/components/Button/Button.test.jsx
+++ src/components/Button/Button.test.jsx
@@ -1,6 +1,6 @@
 import { describe, it, expect } from "vitest";
 import { render, fireEvent } from "@testing-library/react";
-import Button from "./Button";
+import Button from
 
 describe("Testing a button", () => {
   it("Button +1000 is rendered", () => {
@@ -82,4 +82,21 @@ describe("Testing a button", () => {
     expect(count).toBe(1);
     expect(button.textContent).contains("+1");
   });
+
+  it("Button with Infinity prop", () => {
+    let count = 0;
+    
+    const incrementCount = (increment) => {
+      count += increment;
+      return count;
+    };
+    const { container } = render(
+      <Button increment={Infinity} onClickFunction={incrementCount} />
+    );
+    const button = container.querySelector("button");
+    fireEvent.click(button);
+    expect(count).toBe(Infinity); // 0 + Infinity  = Infinity
+    expect(button.textContent).contains("+Infinity");
+  });
+
 });
